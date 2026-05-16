1fc5d22f420ee022b94aaa9f7ef1865acb64a90b
Voltsparx⌁ <voltsparx@gmail.com>
2026-05-17 02:13:16 +0530

    Rename project references in core/interface/command_spec.py

--- core/interface/command_spec.py
+++ core/interface/command_spec.py
@@ -1,15 +1,15 @@
 # ------------------------------------------------------------------------------
 # SPDX-License-Identifier: Proprietary
 #
-# Silica-X Intelligence Framework
+# Silinosic-X Intelligence Framework
 # Copyright (c) 2026 voltsparx
 #
 # Author     : voltsparx
-# Repository : https://github.com/voltsparx/Silica-X
+# Repository : https://github.com/voltsparx/Silinosic-X
 # Contact    : voltsparx@gmail.com
 # License    : See LICENSE file in the project root
 #
-# This file is part of Silica-X and is subject to the terms
+# This file is part of Silinosic-X and is subject to the terms
 # and conditions defined in the LICENSE file.
 # ------------------------------------------------------------------------------
 
@@ -159,7 +159,7 @@ def surface_scan_type_specs() -> tuple[SurfaceScanTypeSpec, ...]:
 
 
 def normalize_surface_scan_type(value: str | None) -> str | None:
-    """Normalize operator scan-type aliases into supported Silica-X scan directives."""
+    """Normalize operator scan-type aliases into supported Silinosic-X scan directives."""
 
     key = str(value or "").strip().lower().replace("_", "-")
     if not key:
