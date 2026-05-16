243dc46a0b39597e5d4d90a95dd1ee908d6cb42b
Voltsparx⌁ <voltsparx@gmail.com>
2026-05-17 02:08:11 +0530

    Rename project references in core/collect/domain_recon.py

--- core/collect/domain_recon.py
+++ core/collect/domain_recon.py
@@ -1,11 +1,11 @@
 # ──────────────────────────────────────────────────────────────
 # SPDX-License-Identifier: Proprietary
 #
-# Silica-X Intelligence Framework
+# Silinosic-X Intelligence Framework
 # Copyright (c) 2026 voltsparx
 #
 # Author     : voltsparx
-# Repository : https://github.com/voltsparx/Silica-X
+# Repository : https://github.com/voltsparx/Silinosic-X
 # Contact    : voltsparx@gmail.com
 # License    : See LICENSE file in the project root
 # ──────────────────────────────────────────────────────────────
