eda78bf0f30996ce2dd856543c7192ce61560be3
Voltsparx⌁ <voltsparx@gmail.com>
2026-05-17 02:06:15 +0530

    Rename silica_x/__main__.py to silinosic_x/__main__.py

--- silica_x/__main__.py
+++
@@ -1,9 +0,0 @@
-"""Module entrypoint for python -m silica_x."""
-
-from __future__ import annotations
-
-from silica_x.cli import main
-
-
-if __name__ == "__main__":
-    main()
---
+++ silinosic_x/__main__.py
@@ -0,0 +1,9 @@
+"""Module entrypoint for python -m silinosic_x."""
+
+from __future__ import annotations
+
+from silinosic_x.cli import main
+
+
+if __name__ == "__main__":
+    main()
